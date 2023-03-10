bdc36bded1962e42391428948e715e1907795edd
heeyame <heeyame@gmail.com>
2023-03-10 10:17:01 +0900

    Boot-2.0.3.RELEASE_Cloud-Finchley.RELEASE
    Spring Boot : 2.0.3.RELEASE
    Spring Cloud : Finchley.RELEASE
    Spring Cloud Stream : 2.0.0.RELEASE

--- spring-cloud-stream-pub/src/test/java/kr/pe/chani/springcloudstreampub/SpringCloudStreamPubApplicationTests.java
+++ spring-cloud-stream-pub/src/test/java/kr/pe/chani/springcloudstreampub/SpringCloudStreamPubApplicationTests.java
@@ -1,13 +1,13 @@
 package kr.pe.chani.springcloudstreampub;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringCloudStreamPubApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringCloudStreamPubApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
